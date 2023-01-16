c086a58126933c7d379b5b3d9814a0e6b8ef5b32
Banut Raul <banut.raul@yahoo.com>
2023-01-16 19:09:30 +0200

    First commit

---
+++ BabyYoda/BabyYoda/BabyYodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyYodaApp.swift
+//  BabyYoda
+//
+//  Created by Raul Banut on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BabyYodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
